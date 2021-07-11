950bd0c8042b303ee583714a3b72f68fd4432f98
Volodymyr <konuhvladimir1@gmail.com>
2021-07-11 14:07:22 +0300

    1

--- src/main/java/com/epam/rd/java/basic/practice3/Part1.java
+++ src/main/java/com/epam/rd/java/basic/practice3/Part1.java
@@ -1,13 +1,27 @@
 package com.epam.rd.java.basic.practice3;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Part1 {
 
+    public static final String string = Util.getInput("part1.txt");
+
     public static void main(String[] args) {
-        
+        System.out.println(Part1.convert1(string));
+        System.out.println(Part1.convert1(string));
+
     }
 
     public static String convert1(String input) {
-        return null;
+        String regex = "(\\S+);(\\S+)\\s(\\S+);((\\S+)@(\\S+))$";
+        Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
+        Matcher matcher = pattern.matcher(string);
+        StringBuilder sb = new StringBuilder();
+        while (matcher.find()) {
+            sb.append(matcher.group(1)).append(": ").append(matcher.group(4)).append(System.lineSeparator());
+        }
+        return sb.toString().trim();
     }
 
     public static String convert2(String input) {
