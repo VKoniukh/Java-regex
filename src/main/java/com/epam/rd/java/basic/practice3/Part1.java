151e37dae8f211eb5247047a0136bf52cd74a0a1
Volodymyr <konuhvladimir1@gmail.com>
2021-07-14 23:52:11 +0300

    14

--- src/main/java/com/epam/rd/java/basic/practice3/Part1.java
+++ src/main/java/com/epam/rd/java/basic/practice3/Part1.java
@@ -47,7 +47,7 @@ public class Part1 {
             sb.deleteCharAt(sb.lastIndexOf(", "));
             n++;
         }
-        sb.replace(25, 26, "");
+        sb.replace(25, 26, (System.lineSeparator()));
         String sb1 = sb.toString().trim();
         return sb1.trim();
     }
