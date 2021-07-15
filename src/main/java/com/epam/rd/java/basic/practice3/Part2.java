9b993655ccf45e9aac32538efc99b0b81af91aee
Volodymyr <konuhvladimir1@gmail.com>
2021-07-15 15:31:03 +0300

    14

--- src/main/java/com/epam/rd/java/basic/practice3/Part2.java
+++ src/main/java/com/epam/rd/java/basic/practice3/Part2.java
@@ -1,20 +1,44 @@
 package com.epam.rd.java.basic.practice3;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class Part2 {
 
     public static void main(String[] args) {
-        
+        String input = Util.getInput("part2.txt");
+        System.out.println(Part2.convert(input));
     }
+
 
     public static String convert(String input) {
-        String regex = "[A-Za-z]+$";
-        Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
-        Matcher matcher = pattern.matcher(input);
-        StringBuilder sb = new StringBuilder();
-
-        return null;
+//        String s = input;
+//        String longest = Arrays.stream(s.split(" ")).max(Comparator.comparingInt(String::length)).orElse(null);
+//        System.out.println(longest);
+//        return longest;
+        if (input == null)
+            return null;
+        String sw = "", lw = "";
+        int s = input.length(), l = 0;
+        String words[] = input.split(" ");
+        for (String word : words) {
+            if (word.length() < s) {
+                sw = word;
+                s = word.length();
+            }
+            if (word.length() > l) {
+                lw = word;
+                l = word.length();
+            }
+        }
+        System.out.println("LONGEST WORD : " + lw);
+        System.out.println("SHORTEST WORD : " + sw);
+        return sw+lw;
     }
 }
+
+
+
+
+
+
+
+
+
--- src/main/java/com/epam/rd/java/basic/practice3/Part5.java
+++ src/main/java/com/epam/rd/java/basic/practice3/Part5.java
@@ -1,8 +1,12 @@
 package com.epam.rd.java.basic.practice3;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Part5 {
 
     public static void main(String[] args) {
+
         
     }
 
@@ -11,6 +15,32 @@ public class Part5 {
     }
 
     public static int roman2Decimal(String roman) {
+        final int [] decimals = {4, 9, 40, 90, 1, 5, 10, 50, 100};
+        final String [] romans = {"IV", "IX", "XL", "XC", "I", "V", "X", "L", "C"};
+        int sum = 0;
+
+        for (int i = 0; i < roman.length(); i++) {
+            for (String s : romans) {
+                Matcher m = Pattern.compile("^" + s).matcher(roman);
+                if (m.find()) {
+                    sum += decimals[findIndex(m.group(), romans)];
+
+                    if (roman.length() - m.group().length() == 0) {
+                        return sum;
+                    }
+                    return sum + roman2Decimal(roman.substring(m.group().length()));
+                }
+            }
+        }
+        return 0;
+    }
+
+    public static int findIndex (String str, String[] romans) {
+        for (int i = 0; i < romans.length; i++) {
+            if (romans[i].equals(str)) {
+                return i;
+            }
+        }
         return 0;
     }
 }
